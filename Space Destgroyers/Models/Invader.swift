43f77527b403e12595aad3554546a7ccc875f21a
Matt Kern <mjkern14@gmail.com>
2019-10-31 21:26:44 -0400

    playing sound with avaudioplayer

--- Space Destgroyers/Models/Invader.swift
+++ Space Destgroyers/Models/Invader.swift
@@ -17,14 +17,15 @@ class Invader: SKSpriteNode{
   // we will determine the invader's row/column later, set to (0,0) for now
   var invaderRow = 0
   var invaderColumn = 0
-  let player = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: "lab9images/beep.mp3"))
+  
+  let player: AVAudioPlayer
   
   
   let sound = SKAudioNode(fileNamed: "lab9images/beep.mp3")
   override var position : CGPoint {
     didSet {
       sound.position = position
-      player.pan = Float(position.x/UIScreen.main.bounds.width)
+      //player.pan = Float(position.x/UIScreen.main.bounds.width)
     }
   }
   
@@ -33,21 +34,36 @@ class Invader: SKSpriteNode{
     // we have three types of invader images so randomly chose among these
     let randNum = Int(arc4random_uniform(3) + 1)
     let texture = SKTexture(imageNamed: "invader\(randNum)")
-    super.init(texture: texture, color: SKColor.clear, size: texture.size())
-    self.name = "invader"
+    
+    
     
     // matt - make invaders have positional sound
     sound.isPositional = true
     //addChild(sound)
     
-    player.play()
+    //player.play()
     
+    // this works now for no known reason:
+    let path = Bundle.main.path(forResource: "lab9images/beep.mp3", ofType: nil)!
+    let url = URL(fileURLWithPath: path)
+    player = try! AVAudioPlayer(contentsOf: url)
+    player.numberOfLoops = -1 // makes it loop forever
+    player.play()
+//    player = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: "beep-1"))
+    
+    
+    super.init(texture: texture, color: SKColor.clear, size: texture.size())
+    self.name = "invader"
     // preparing invaders for collisions once we add physics...
     
   }
   
   required init?(coder aDecoder: NSCoder) {
     // SKSpriteNode conforms to NSCoding, which requires we implement this, but we can just call super.init()
+    
+    let path = Bundle.main.path(forResource: "beep.mp3", ofType:nil)!
+    let url = URL(fileURLWithPath: path)
+    player = try! AVAudioPlayer(contentsOf: url)
     super.init(coder: aDecoder)
   }
   
