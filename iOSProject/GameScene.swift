7befaef1554e4ef2b2857b2dfca70924a32dd014
Matt Kern <mjkern14@gmail.com>
2019-11-02 12:50:52 -0400

    tried to purge space destroyers

---
+++ iOSProject/GameScene.swift
@@ -0,0 +1,80 @@
+//
+//  GameScene.swift
+//  Space Destgroyers
+//
+//  Created by Matt Kern on 10/26/19.
+//  Copyright © 2019 Matt Kern. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+import CoreMotion
+
+var levelNum = 1
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+  
+  let audioManager = AudioManager()
+  var bat : Bat? = nil
+  var rider: Rider? = nil
+  let maxLevels = 3
+  var motionManager: CMMotionManager = CMMotionManager()
+  override func didMove(to view: SKView) {
+    
+    backgroundColor = SKColor.black
+      
+    bat = Bat(audioManager: audioManager)
+    rider = Rider(audioManager: audioManager)
+    addChild(bat!)
+    addChild(rider!)
+  
+  }
+  
+  func touchDown(atPoint pos : CGPoint) {
+    
+  }
+  
+  func touchMoved(toPoint pos : CGPoint) {
+    
+  }
+  
+  func touchUp(atPoint pos : CGPoint) {
+    
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    bat!.position = touches.first?.location(in: self) as! CGPoint
+  }
+  
+  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    
+  }
+  
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    rider!.position = touches.first?.location(in: self) as! CGPoint
+  }
+  
+  override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    
+  }
+  
+  override func update(_ currentTime: TimeInterval) {
+      // Called before each frame is rendered
+  }
+  
+  // MARK: - Game Management Methods
+  func levelComplete(){
+    if(levelNum <= maxLevels){
+    } else {
+      levelNum = 1
+      newGame()
+    }
+  }
+  
+  func newGame(){
+    let gameOverScene = StartGameScene(size: size)
+    gameOverScene.scaleMode = scaleMode
+    let transitionType = SKTransition.flipHorizontal(withDuration: 0.5)
+    view?.presentScene(gameOverScene,transition: transitionType)
+  }
+}
