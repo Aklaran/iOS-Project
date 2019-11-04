42a80d02655d42e5590536a70c78665b8cd7fe67
Dean Dijour <dijour@cmu.edu>
2019-11-04 12:47:32 -0500

    added hearts and landing page graphic

--- iOSProject/GameScene.swift
+++ iOSProject/GameScene.swift
@@ -20,12 +20,30 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
   var bats : [Bat] = [Bat]()
   var rider: Rider? = nil
   let maxLevels = 3
+  let maxLives = 3;
+  var lives = [SKSpriteNode]();
   var motionManager: CMMotionManager = CMMotionManager()
   override func didMove(to view: SKView) {
-    
     backgroundColor = SKColor.black
+    
+    let background = SKSpriteNode(imageNamed: "background")
+    background.anchorPoint = CGPoint(x: 0.5, y: 0)
+    background.position = CGPoint(x: size.width/2, y: 0)
+    background.size.height = self.frame.size.height;
+    addChild(background)
+    
     rider = Rider(audioManager: audioManager)
     addChild(rider!)
+    
+    for var i in 0 ..< maxLives {
+      var newHeart = SKSpriteNode(imageNamed: "heart");
+      newHeart.anchorPoint = (CGPoint(x: 0.5, y: 0.5))
+      newHeart.position = CGPoint(x: CGFloat(i * 100), y: (size.height - newHeart.size.height));
+      lives.append(newHeart)
+      addChild(newHeart)
+      print(i)
+      i += 1
+    }
     
     // for spawning the bats
     let wait = SKAction.wait(forDuration: 3, withRange: 2)
