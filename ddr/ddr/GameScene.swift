842e59f203d6a573ba23dd3c4afeadb0e08581a5
Bo Tembunkiart <botbunk@gmail.com>
2019-11-04 16:51:51 -0500

    refactors some of gamescene

--- ddr/ddr/GameScene.swift
+++ ddr/ddr/GameScene.swift
@@ -18,46 +18,57 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
   
   let maxLevels = 3
   
-  var lives = [SKSpriteNode]();
-  
   let audioManager = AudioManager()
   let motionManager = CMMotionManager()
   
-  var bats = [Bat]()
+  var lives = [SKSpriteNode]();
   
+  var bats = [Bat]()
   var rider: Rider? = nil
   
   override func didMove(to view: SKView) {
+    initializeBackground()
     
+    initializeRider()
+    
+    initializeHearts()
+    
+    beginSpawningBats()
+  }
+  
+  func initializeBackground() {
     let background = SKSpriteNode(imageNamed: "background")
     background.anchorPoint = CGPoint(x: 0.5, y: 0)
     background.position = CGPoint(x: size.width/2, y: 0)
     background.size.height = self.frame.size.height;
     addChild(background)
-    
+  }
+  
+  func initializeRider() {
     rider = Rider(audioManager: audioManager, motionManager: motionManager)
     addChild(rider!)
-    
-    for var i in 0 ..< rider!.lives {
-       var newHeart = SKSpriteNode(imageNamed: "heart");
-       newHeart.anchorPoint = (CGPoint(x: 0.5, y: 0.5))
-       newHeart.position = CGPoint(x: CGFloat(i * 100), y: (size.height - newHeart.size.height));
-       lives.append(newHeart)
-       addChild(newHeart)
-       print(i)
-       i += 1
-     }
-    
-    // for spawning the bats
-    let wait = SKAction.wait(forDuration: 5, withRange: 2)
-    let spawn = SKAction.run {
-      let bat = Bat(audioManager: self.audioManager)
-      self.bats.append(bat)
-      self.addChild(bat)
+  }
+  
+  func initializeHearts() {
+    for i in 0 ..< rider!.lives {
+      let newHeart = SKSpriteNode(imageNamed: "heart");
+      newHeart.anchorPoint = (CGPoint(x: 0.5, y: 0.5))
+      newHeart.position = CGPoint(x: CGFloat(i * 100), y: (size.height - newHeart.size.height));
+      lives.append(newHeart)
+      addChild(newHeart)
     }
-    let sequence = SKAction.sequence([wait, spawn])
-//    run(spawn) // for testing only generate one bat
-    run(SKAction.repeatForever(sequence))
+  }
+  
+  func beginSpawningBats() {
+      let wait = SKAction.wait(forDuration: 5, withRange: 2)
+      let spawn = SKAction.run {
+        let bat = Bat(audioManager: self.audioManager)
+        self.bats.append(bat)
+        self.addChild(bat)
+      }
+      let sequence = SKAction.sequence([wait, spawn])
+      // run(spawn) // for testing only generate one bat
+      run(SKAction.repeatForever(sequence))
   }
   
   func touchDown(atPoint pos : CGPoint) {
@@ -119,9 +130,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
   func checkCollision(bat: Bat, rider: Rider) {
     let batThird = floor(bat.position.x / THIRD_SCREEN_WIDTH)
     let riderThird = floor(rider.x / THIRD_SCREEN_WIDTH)
-    
-    print("bat third: ", batThird)
-    print("rider third: ", riderThird)
     
     if batThird == riderThird {
       // play hit sound, decrement lives
